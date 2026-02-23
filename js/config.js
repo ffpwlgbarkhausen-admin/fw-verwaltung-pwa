e579ead6f55b6df3127ca02b0f8e0012b29b89ba
ffpwlgbarkhausen-admin <ffpw.lgbarkhausen@gmail.com>
2026-02-23 14:50:30 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,19 +3,7 @@
  */
 const CONFIG = {
     API_URL: "https://script.google.com/macros/s/AKfycbyja4HWHpmuwWTuZBEXTOFGns8-xb1S4ppSUc2xXWcJ8SdBmfkUZQUlSa-z-ypqK6jW/exec",
-    JUBILAEEN: [25, 35, 40, 50, 60, 70, 75, 80],
-    LEHRGAENGE: [
-        "Probezeit", 
-        "Grundausbildung", 
-        "Truppführer", 
-        "Gruppenführer", 
-        "Zugführer", 
-        "Verbandsführer 1", 
-        "Verbandsführer 2"
-    ],
-    THEME: {
-        AMBER: 'amber-500',
-        GREEN: 'green-600',
-        RED: 'red-700'
-    }
+    JUBILAEEN: [10, 25, 40, 50, 60, 70],
+    LEHRGAENGE: ["Grundausbildung", "Truppführer", "Gruppenführer", "Zugführer", "Verbandsführer 1", "Verbandsführer 2"],
+    THEME: { AMBER: 'amber-500', GREEN: 'green-600', RED: 'red-700' }
 };
